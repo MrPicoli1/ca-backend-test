16ee60bdd84efcefe99e8cf5206a86da2096ecd1
Guilherme Picoli <83010918+MrPicoli1@users.noreply.github.com>
2024-06-28 14:09:43 -0300

    Billing Tests

---
+++ BackendTest.Test/Controllers/BillingControllerTests.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Net.Http.Json;
+using System.Net;
+using System.Xml.Linq;
+
+namespace BackendTest.Test.Controllers
+{
+    [TestClass]
+    public class BillingControllerTests
+    {
+        [TestMethod]
+        [TestCategory("Controller")]
+        [DataRow("")]
+        [DataRow("2")]
+        [DataRow("3")]
+        public async Task Should_return_200_if_valid_billing(string id)
+        {
+            var api = new ApiFactory();
+            var httpClient = api.CreateClient();
+
+            var response = await httpClient.PostAsJsonAsync("/billing}", new
+            {
+                Id = $"{id}"
+            });
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+        [TestMethod]
+        [TestCategory("Controller")]
+        [DataRow("1")]
+        [DataRow("")]
+        [DataRow("")]
+        public async Task Shoud_return_400_if_invalid_billing(string id)
+        {
+            var api = new ApiFactory();
+            var httpClient = api.CreateClient();
+
+            var response = await httpClient.PostAsJsonAsync("/billing}", new
+            {
+                Id = $"{id}"
+            });
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
--- BackendTest.Test/obj/Debug/net8.0/BackendTest.Test.AssemblyInfo.cs
+++ BackendTest.Test/obj/Debug/net8.0/BackendTest.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendTest.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8616088dd0d982ad7af888a5586367a78468cc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d435b98be0ff846e15556c33f39e33b3e09d0be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendTest.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendTest.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
